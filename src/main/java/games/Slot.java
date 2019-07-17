a8e106e2246108ecfff3c3c427ef4a951a207cda
Dmitry <bdn.bespalov@gmail.com>
2019-07-17 10:13:14 +0300

    Шаг №4 - Первая игра - "Однорукий бандит"

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -1,10 +1,36 @@
 package main.java.games;
 
 public class Slot {
+    //счет
+    int my_money=100;
+    //ставка
+    private final int RATE=10;
+
+    int size=7;
+    int firstCounter=0;
+    int secondCounter=0;
+    int thirdCounter=0;
+
+    Slot(){
+        System.out.println("У Вас "+my_money+"$, ставка - 10$");
+        for(int i=0;i<my_money;i++){
+            firstCounter = (firstCounter + (int) Math.round(Math.random() * 100)) % size;
+            secondCounter = (secondCounter + (int) Math.round(Math.random() * 100)) % size;
+            thirdCounter = (thirdCounter + (int) Math.round(Math.random() * 100)) % size;
+            System.out.println("Крутим барабаны! Розыгрыш принёс следующие результаты:");
+            System.out.println("первый барабан - "+firstCounter+", второй - "+secondCounter+", третий - "+thirdCounter);
+            if(thirdCounter == secondCounter && secondCounter == firstCounter){
+                my_money+=1000;
+                System.out.println("Выйгрыш 1000$, ваш капитал теперь составляет: "+my_money+"$");
+                return;
+            }else {
+                my_money-=RATE;
+                System.out.println("Проигрыш 10$, ваш капитал теперь составляет: "+my_money+"$");
+            }
+        }
+    }
+
     public static void main(String... __) {
-        System.out.println("Hello, World!");
-
-
-
+        Slot slot=new Slot();
     }
 }
